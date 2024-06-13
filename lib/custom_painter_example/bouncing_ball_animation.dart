3375aac14e7be9ca0ab6e992864d50abe39d70ea
ahmadli-codex <ibrahim.ahmadli06@outlook.com>
2024-06-13 20:02:50 +0400

    Animations Practice

--- lib/custom_painter_example/bouncing_ball_animation.dart
+++ lib/custom_painter_example/bouncing_ball_animation.dart
@@ -7,18 +7,94 @@ class BouncingBallAnimation extends StatefulWidget {
   State<BouncingBallAnimation> createState() => _BouncingBallAnimationState();
 }
 
-class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
+class _BouncingBallAnimationState extends State<BouncingBallAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    animation = Tween<double>(begin: 0, end: 1).animate(controller);
+
+    animation.addListener(() {
+      if (animation.status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (animation.status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            // Need Ball
+            AnimatedBuilder(
+                animation: animation,
+                builder: (context, child) {
+                  return CustomPaint(
+                    size: MediaQuery.of(context).size / 2,
+                    painter: BouncingBallPainter(animation.value),
+                  );
+                }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(100, 100),
+                      ),
+                      onPressed: () {
+                        if (controller.status == AnimationStatus.forward ||
+                            controller.status == AnimationStatus.reverse) {
+                          controller.stop();
+                        } else {
+                          controller.reset();
+                        }
+                      },
+                      label: const Icon(
+                        Icons.stop,
+                        size: 40,
+                      )),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class BouncingBallPainter extends CustomPainter {
+  final double animationValue;
+  BouncingBallPainter(this.animationValue);
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(
+        Offset(size.width * 0.5,
+            (size.height - (size.height * animationValue)) / 2),
+        20,
+        Paint()..color = Colors.blue);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:animation_starter_code/custom_painter_example/bouncing_ball_animation.dart';
+import 'package:animation_starter_code/explicit_examples/list_animation.dart';
+import 'package:animation_starter_code/explicit_examples/loading_animation.dart';
+import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:animation_starter_code/implicit_examples/animated_color_pallete.dart';
+import 'package:animation_starter_code/implicit_examples/animated_shopping_cart_button.dart';
+import 'package:animation_starter_code/implicit_examples/animated_tween_animation_builder_example.dart';
+import 'package:animation_starter_code/page_route_builder_animation/splash_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AnimatedColorPalette(),
+      home: const BouncingBallAnimation(),
     );
   }
 }
